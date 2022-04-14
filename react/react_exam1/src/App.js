c82ac363646abdeb504d1903e023d42266caf40f
hj5o <dataliteracy@naver.com>
2022-04-14 22:24:14 +0900

    React.js 기초

--- react/react_exam1/src/App.js
+++ react/react_exam1/src/App.js
@@ -1,29 +1,31 @@
 import './App.css';
-import MyHeader from './MyHeader';
-import MyFooter from './MyFooter';
+import MyHedaer from './MyHeader';
+import Counter from './Counter';
+import Container from './Container';
 
 function App() {
-  let name = "안녕";
 
-  // const style = {          inline styling
-  //   App : {
-  //     backgroundColor: 'black'
-  //   }
-  // }
-
-  const num = 5;
-
+  const counterProps = {
+      a:1,
+      b:2,
+      c:3,
+      d:4,
+      e:5,
+      // initialValue:5
+  }
   return (
-    <div className="App">
-    {/* <div style={style.App}> */}
-      <MyHeader />
-      <h2>안녕{name} 리액트</h2>
-      <b id="bold_text">React.js</b> <br/>
-      <b id="bold_text">{num}는 : {num % 2 === 0 ? "짝수" : "홀수"}
-      </b>
-      <MyFooter />
+    <Container>
+    <div>
+      <MyHedaer />
+      <Counter {...counterProps} />
+      {/* prop << 초기값 지정 */}
     </div>
+    </Container>
   );
+}
+
+Counter.defaultProps={
+  initialValue:0
 }
 
 export default App;
